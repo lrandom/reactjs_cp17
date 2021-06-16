8b7631948b40bef77b0af5e64bca307d71176097
root <luann4099@gmail.com>
2021-06-16 09:58:09 +0700

    update bt3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ Tìm hiểu trước về JSX - cú pháp cơ bản  // vừa dạy vừa hỏi
 
 
 /*
-Viết chương trình xem ảnh, cho một tập danh sách các ảnh như sau 
+BT1 - Viết chương trình xem ảnh, cho một tập danh sách các ảnh như sau 
 const gallery = [
   "https://img.nhandan.com.vn/Files/Images/2020/07/26/nhat_cay-1595747664059.jpg",
   "https://img.nhandan.com.vn/Files/Images/2020/07/26/giai_thuong_lon-1595747403778.jpg"
@@ -15,6 +15,10 @@ const gallery = [
 ]
 tạo một giao diện dạng lưới và tải các ảnh lên lưới đó, khi người dùng click vào ảnh thì show to ảnh ra
 ở giao diện phóng to sẽ có một nút close, khi click vào nút đó thì đóng giao diện phóng to 
+*/
+
+/*
+BT2 - Từ link ảnh trên, hãy viết một slide xem ảnh bằng reactjs
 */
 
 //import logo from './logo.svg';
